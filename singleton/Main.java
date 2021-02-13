2bc451d8955a6dfc13de095951aac113cdf96215
Hanme Hwang <hanme@linecorp.com>
2021-02-13 23:06:32 +0900

    Add singleton example

--- singleton/Main.java
+++ singleton/Main.java
@@ -2,14 +2,12 @@ package singleton;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Start.");
-        Singleton obj1 = Singleton.getInstance();
-        Singleton obj2 = Singleton.getInstance();
-        if (obj1 == obj2) {
-            System.out.println("obj1 and obj2 are same instance");
-        } else {
-            System.out.println("obj1 and obj2 are differenc instances");
-        }
-        System.out.println("End.");
+        TicketMaker t1 = TicketMaker.getInstance();
+        TicketMaker t2 = TicketMaker.getInstance();
+        int n1 = t1.getNextTicketNumber();
+        int n2 = t2.getNextTicketNumber();
+        int n3 = t1.getNextTicketNumber();
+        int n4 = t2.getNextTicketNumber();
+        System.out.println(n1 + " " + n2 + " " + n3 + " " + n4);
     }
 }
---
+++ singleton/TicketMaker.java
@@ -0,0 +1,18 @@
+package singleton;
+
+public class TicketMaker {
+    private int ticket = 1000;
+    private static TicketMaker ticketMaker = new TicketMaker();
+
+    private TicketMaker() {
+        System.out.println("Create TicketMaker");
+    }
+
+    public static TicketMaker getInstance() {
+        return ticketMaker;
+    }
+
+    public synchronized int getNextTicketNumber() {
+        return ticket++;
+    }
+}
