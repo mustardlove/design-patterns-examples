85bc174cb8f47bcead08650f592a0505d0421900
Hanme Hwang <hanme@linecorp.com>
2021-02-14 15:01:56 +0900

    Add headfirst decorator pattern example

---
+++ decorator/headfirst/Soy.java
@@ -0,0 +1,19 @@
+package decorator.headfirst;
+
+public class Soy extends CondimentDecorator {
+    Beverage beverage;
+
+    public Soy(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public double cost() {
+        return .15 + beverage.cost();
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", Soy";
+    }
+}
---
+++ decorator/headfirst/Whip.java
@@ -0,0 +1,19 @@
+package decorator.headfirst;
+
+public class Whip extends CondimentDecorator {
+    Beverage beverage;
+
+    public Whip(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public double cost() {
+        return .10 + beverage.cost();
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", Whip";
+    }
+}
