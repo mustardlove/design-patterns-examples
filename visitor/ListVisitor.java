3119b0ef52c1169d0f953f57c8f8561bbad49b8e
Hanme Hwang <hanme@linecorp.com>
2021-02-24 23:29:31 +0900

    Add code to visitor

--- visitor/ListVisitor.java
+++ visitor/ListVisitor.java
@@ -1,4 +1,11 @@
 package visitor;
 
 public class ListVisitor extends Visitor {
+    @Override
+    public void visit(File file) {
+    }
+
+    @Override
+    public void visit(Directory directory) {
+    }
 }
