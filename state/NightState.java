2b1345bb15acdd1a2f5df9bedfa10ee788f7b1d3
Hanme Hwang <hanme@linecorp.com>
2021-03-07 12:50:40 +0900

    Add code to state example

--- state/NightState.java
+++ state/NightState.java
@@ -12,21 +12,27 @@ public class NightState implements State {
 
     @Override
     public void doClock(Context context, int hour) {
-
+        if (9 <= hour && hour < 17) {
+            context.changeState(DayState.getInstance());
+        }
     }
 
     @Override
     public void doUse(Context context) {
-
+        context.callSecurityCenter("Emergency: safe use at night!");
     }
 
     @Override
     public void doAlarm(Context context) {
-
+        context.callSecurityCenter("Emergency Bell (night)");
     }
 
     @Override
     public void doPhone(Context context) {
+        context.recordLog("night call recording");
+    }
 
+    public String toString() {
+        return "[night]";
     }
 }
