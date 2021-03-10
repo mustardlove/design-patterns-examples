04d28d32264ede7b38b846521d4efe3c3ae51c73
Hanme Hwang <hanme@linecorp.com>
2021-03-10 23:34:32 +0900

    Complete state example

--- state/SafeFrame.java
+++ state/SafeFrame.java
@@ -15,31 +15,73 @@ public class SafeFrame extends Frame implements ActionListener, Context {
     private State state = DayState.getInstance();
 
     public SafeFrame(String title) {
+        super(title);
+        setBackground(Color.lightGray);
+        setLayout(new BorderLayout());
 
+        add(textClock, BorderLayout.NORTH);
+        textClock.setEditable(false);
+
+        add(textScreen, BorderLayout.CENTER);
+        textScreen.setEditable(false);
+
+        Panel panel = new Panel();
+        panel.add(buttonUse);
+        panel.add(buttonAlarm);
+        panel.add(buttonPhone);
+        panel.add(buttonExit);
+
+        add(panel, BorderLayout.SOUTH);
+
+        pack();
+        show();
+
+        buttonUse.addActionListener(this);
+        buttonAlarm.addActionListener(this);
+        buttonPhone.addActionListener(this);
+        buttonExit.addActionListener(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
-
+        System.out.println(actionEvent.toString());
+        if (actionEvent.getSource() == buttonUse) {
+            state.doUse(this);
+        } else if (actionEvent.getSource() == buttonAlarm) {
+            state.doAlarm(this);
+        } else if (actionEvent.getSource() == buttonExit) {
+            System.exit(0);
+        } else {
+            System.out.println("?");
+        }
     }
 
     @Override
     public void setClock(int hour) {
-
+        String clockstring = "Current Time";
+        if (hour < 10) {
+            clockstring += "0" + hour + ":00";
+        } else {
+            clockstring += hour + ":00";
+        }
+        System.out.println(clockstring);
+        textClock.setText(clockstring);
+        state.doClock(this, hour);
     }
 
     @Override
     public void changeState(State state) {
-
+        System.out.println(this.state + " -> " + state);
+        this.state = state;
     }
 
     @Override
     public void callSecurityCenter(String msg) {
-
+        textScreen.append("call! " + msg + "\n");
     }
 
     @Override
     public void recordLog(String msg) {
-
+        textScreen.append("record ... " + msg + "\n");
     }
 }
