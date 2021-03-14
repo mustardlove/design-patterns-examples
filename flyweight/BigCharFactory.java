127b24f8e9e809b83ec62c2a3d84b861f6db8bea
Hanme Hwang <hanme@linecorp.com>
2021-03-14 23:55:28 +0900

    Add more code to flyweight - 2

--- flyweight/BigCharFactory.java
+++ flyweight/BigCharFactory.java
@@ -1,4 +1,13 @@
 package flyweight;
 
 public class BigCharFactory {
+    private static BigCharFactory singleton = new BigCharFactory();
+
+    public static BigCharFactory getInstance() {
+        return singleton;
+    }
+
+    public synchronized BigChar getBigChar(char charAt) {
+        return null;
+    }
 }
--- flyweight/BigString.java
+++ flyweight/BigString.java
@@ -4,6 +4,11 @@ public class BigString {
     private BigChar[] bigchars;
 
     public BigString(String string) {
+        bigchars = new BigChar[string.length()];
+        BigCharFactory factory = BigCharFactory.getInstance();
+        for (int i = 0; i < bigchars.length; i++) {
+            bigchars[i] = factory.getBigChar(string.charAt(i));
+        }
     }
 
     public void print() {
