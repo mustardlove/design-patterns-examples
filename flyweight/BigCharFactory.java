1f24ba00642792739beaa03dfe2ff42db0d37dde
Hanme Hwang <hanme@linecorp.com>
2021-03-15 22:15:16 +0900

    Complete flyweight example

--- flyweight/BigCharFactory.java
+++ flyweight/BigCharFactory.java
@@ -1,13 +1,21 @@
 package flyweight;
 
+import java.util.HashMap;
+
 public class BigCharFactory {
+    private HashMap pool = new HashMap();
     private static BigCharFactory singleton = new BigCharFactory();
 
     public static BigCharFactory getInstance() {
         return singleton;
     }
 
-    public synchronized BigChar getBigChar(char charAt) {
-        return null;
+    public synchronized BigChar getBigChar(char charname) {
+        BigChar bc = (BigChar)pool.get("" + charname);
+        if (bc == null) {
+            bc = new BigChar(charname);
+            pool.put("" + charname, bc);
+        }
+        return bc;
     }
 }
