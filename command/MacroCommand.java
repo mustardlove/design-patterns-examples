1adfb9fea1866b80f146db874e00e2c3e850ce39
Hanme Hwang <hanme@linecorp.com>
2021-03-19 23:27:45 +0900

    Add more code to command pattern

--- command/MacroCommand.java
+++ command/MacroCommand.java
@@ -1,8 +1,16 @@
 package command;
 
+import java.util.Iterator;
+import java.util.Stack;
+
 public class MacroCommand implements Command {
+    private Stack<Command> commands = new Stack<>();
+
     @Override
     public void execute() {
-
+        Iterator it = commands.iterator();
+        while (it.hasNext()) {
+            ((Command)it.next()).execute();
+        }
     }
 }
